3411eae07ec4a1fcf8fe22c362b7bd90dea72a1c
c-cal <github-2c7c@zebrina.fr>
2019-11-02 02:54:46 +0100

    Updates

---
+++ precache-manifest.be957694bd018f8021cbe7c6b914291f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c2c80c2dfabde1acdd8a2b22504ae9",
+    "url": "/tiny_matrix_client/index.html"
+  },
+  {
+    "revision": "5e66719a00bd3f12ca9e",
+    "url": "/tiny_matrix_client/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0c0f445a6f04e4ab1817",
+    "url": "/tiny_matrix_client/static/js/2.f7deeb1a.chunk.js"
+  },
+  {
+    "revision": "5e66719a00bd3f12ca9e",
+    "url": "/tiny_matrix_client/static/js/main.b0630e8e.chunk.js"
+  },
+  {
+    "revision": "1c777e58a374e03c72c7",
+    "url": "/tiny_matrix_client/static/js/runtime-main.d47b2a6b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/tiny_matrix_client/static/media/logo.25bf045c.svg"
+  }
+]);
